9cb2cc00819a1b562f1f1e594f60e9728849494e
ginni-05 <hemangi.sagbhor23@spit.ac.in>
2026-02-21 15:15:49 +0530

    added aiRoutes and aiServices

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
+const aiRoutes = require("./routes/aiRoutes");
 const app = express();
 
 connectDB();
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
   res.send("SitePilot API Running");
 });
 
+app.use("/ai", aiRoutes);
+
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
+
